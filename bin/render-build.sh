0344ff65900bb8914abf615849d12bc7884fab87
Kasaija Kenneth <kasaijak@gmail.com>
2023-03-17 01:42:04 +0300

    Add render.yaml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 # exit on error
+# set -o errexit
+
+# bundle install
+# bundle exec rake db:migrate
+
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle exec rake db:migrate
