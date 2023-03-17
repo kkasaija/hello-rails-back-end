fb2afb5f54096d1025276cee0bfd26c2082f20c9
Kasaija Kenneth <kasaijak@gmail.com>
2023-03-17 03:14:17 +0300

    Update controllers

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 # exit on error
-# set -o errexit
-
-# bundle install
-# bundle exec rake db:migrate
-
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
-
 bundle install
-# bundle exec rake assets:precompile
-# bundle exec rake assets:clean
 bundle exec rake db:migrate
